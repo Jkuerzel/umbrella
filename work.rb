361070a42b16bed4b6ded16f09919d694f45c741
Jkuerzel <jkuerzel@chicagobooth.edu>
2024-10-17 02:25:59 +0000

    Finished Umbrella

--- work.rb
+++ work.rb
@@ -1,30 +1,25 @@
 # /env_test.rb
 require "dotenv/load"
-
-#ENV.fetch("GMAPS_KEY")
-#ENV.fetch("OPENAI_KEY")
-
-#puts "========================================
-#    Will you need an umbrella today?    
-#========================================"
-#puts "\n\nWhere are you\n"
-
-#location= gets.chomp
-
-location="Kassel"
-
-
-# Pull in the HTTP class
+require "daru"
+require "date"
 require "http"
 require "json"
 
+puts "========================================
+    Will you need an umbrella today?    
+========================================"
+puts "\n\nWhere are you?\n"
+
+location= gets.chomp
+
+#Getting the current time
 current_time = Time.now
 # Normalize to the current hour and add one hour if it's not exactly on the hour
 next_full_hour = current_time + (3600 - (current_time.min * 60 + current_time.sec))
-
 # Convert it to a timestamp for parsing
 next_full_hour_timestamp = next_full_hour.to_i
 
+#First Fetching the Coordinates of the Given Location
 
 raw_response=HTTP
   .follow(strict: false)
@@ -43,51 +38,87 @@ raw_response=HTTP
     }
   )
 
-
  
  parsed_response = JSON.parse(raw_response)
- 
+
  results_data=parsed_response.fetch("results")
  geometry=results_data.at(0).fetch("geometry").fetch("location")
 
  latitude=geometry.fetch("lat")
  longitude=geometry.fetch("lng")
-
-
+ # Formatting the output for Input in Pirate Weather
  location_data="/#{latitude},#{longitude}"
  
+ 
+#Now getting the weather data from Pirate Weather
 
-
- # Hidden variables
 pirate_weather_api_key = ENV["PIRATE_WEATHER_KEY"]
-
 # Assemble the full URL string by adding the first part, the API token, and the last part together
 pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + location_data
-
 # Place a GET request to the URL
 raw_response = HTTP.get(pirate_weather_url)
-
 parsed_response = JSON.parse(raw_response)
-#pp parsed_response.keys
 
 #Getting the current temperature
 currently_hash = parsed_response.fetch("currently")
 temperature=currently_hash.fetch("temperature")
+temperature_C=(temperature-32)*5/9
 
 #Getting the Weather Forecast
 hourly = parsed_response.fetch("hourly").fetch("data")
 
-pp hourly.at(0).keys
-
-
+#Setting up variables for storage
+next_hours=[]
+next_summaries=[]
+next_precip_propability=[]
+next_precip_propability10=[]
 
 hourly.each do |data_hash|
-  if data_hash.fetch("time") == next_full_hour_timestamp
-    next_hour = data_hash.fetch("summary")
-    pp data_hash.fetch("summary")
-    pp "I saved a forecast"
+  if data_hash.fetch("time") >= next_full_hour_timestamp && data_hash.fetch("time") < next_full_hour_timestamp +12*3600
+    next_hours.push(data_hash.fetch("time")) 
+    next_summaries.push(data_hash.fetch("summary"))
+    next_precip_propability.push(data_hash.fetch("precipProbability"))
+    if data_hash.fetch("precipProbability")>0.1
+      next_precip_propability10.push(true)
+    else
+      next_precip_propability10.push(false)
+    end
+  elsif data_hash.fetch("time") >= next_full_hour_timestamp +12*3600
     break  # Exit the loop once the condition is met
   end
 end
 
-pp next_hour
+# Convert Unix timestamps to human-readable date and time (including hours)
+formatted_times = next_hours.map { |timestamp| Time.at(timestamp).strftime("%m-%e-%y %H:%M") }
+
+df = Daru::DataFrame.new({
+  "Time" => formatted_times,
+  "Summary" => next_summaries,
+  "Propability" =>next_precip_propability,
+  "Raining" => next_precip_propability10,
+})
+
+
+rains_next_12_hours=false
+time_rain_starts=Date.new
+counter=0
+
+
+puts "Checking the weather at #{location}...."
+puts "Your coordinates are #{latitude}, #{longitude}."
+puts "It is currently #{temperature_C.round(2)}°C or #{temperature.round(2)}°F."
+puts "Next hour: #{df['Summary'][0]}"
+
+df["Raining"].each do |check|
+  if check==true
+    puts "In #{counter+1} hours, there is a #{(df['Propability'][counter]*100).to_i}% chance of precipitation."
+  end
+  counter=counter+1
+end
+
+
+if rains_next_12_hours==false
+  puts "You probably won't need an umbrella."
+else
+  puts "You might want to take an umbrella!"
+end
